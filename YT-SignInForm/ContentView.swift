684d5900c569f6f55e92ad7a8b3697e84b8833d4
Yaroslav Ark <fast.yarik@gmail.com>
2024-01-28 18:48:07 +0200

    Initial Commit

---
+++ YT-SignInForm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YT-SignInForm
+//
+//  Created by Yaroslav on 28.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YT-SignInForm/YT_SignInFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  YT_SignInFormApp.swift
+//  YT-SignInForm
+//
+//  Created by Yaroslav on 28.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct YT_SignInFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
